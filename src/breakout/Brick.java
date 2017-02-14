a9363265d4714b4cd1898e25267343786e6a45e4
Joseph Seder <sederj@mail.gvsu.edu>
2017-02-13 19:17:12 -0500

    Updated Brick class

--- src/breakout/Brick.java
+++ src/breakout/Brick.java
@@ -1,5 +1,11 @@
 package breakout;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
 public class Brick {
+	
+	/** Dimensions of brick object */
+	private static final int HEIGHT = 10, WIDTH = 60;
 
 }
