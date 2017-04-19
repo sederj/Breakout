b29cd9aa6620ff60df160a2c0134847cc394d232
wildscotsmen <thewildscotsmen@gmail.com>
2017-04-19 19:49:42 -0400

    Fixed a CheckStyle error.

--- src/breakout/Score.java
+++ src/breakout/Score.java
@@ -122,8 +122,13 @@ public class Score implements Serializable, Comparable<Score> {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result 
-				+ ((name == null) ? 0 : name.hashCode());
+		int var = 0;
+		if (name == null) {
+			var = 0;
+		} else {
+			var = name.hashCode();
+		}
+		result = prime * result + var;
 		result = prime * result + time;
 		result = prime * result + value;
 		return result;
